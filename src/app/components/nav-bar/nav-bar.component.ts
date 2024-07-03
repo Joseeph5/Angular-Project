c97975ca1cdf96ffc71d485a4ff8ef7bf900279b
Youssef Benamor <youssef.beenamor@gmail.com>
2024-07-03 13:59:11 +0100

    update NavBarComponent

--- src/app/components/nav-bar/nav-bar.component.ts
+++ src/app/components/nav-bar/nav-bar.component.ts
@@ -1,12 +1,28 @@
-import { Component } from '@angular/core';
-
+// src/app/components/layout/nav-bar/nav-bar.component.ts
+import { Component, OnInit } from '@angular/core';
+import { Router, RouterLink } from '@angular/router';
+import { NgIf } from '@angular/common';
 @Component({
   selector: 'app-nav-bar',
   standalone: true,
-  imports: [],
   templateUrl: './nav-bar.component.html',
-  styleUrl: './nav-bar.component.css'
+  styleUrls: ['./nav-bar.component.css'],
+  imports: [RouterLink, NgIf],
 })
-export class NavBarComponent {
+export class NavBarComponent implements OnInit {
+  showAccount: boolean = false;
+  isAdmin: boolean = false;
+  isLoggedIn: boolean = false;
 
+  constructor(private router: Router) {}
+
+  ngOnInit(): void {}
+
+  toggleAccount(): void {
+    this.showAccount = !this.showAccount;
+  }
+
+  logout(): void {
+    this.router.navigate(['/login']);
+  }
 }
