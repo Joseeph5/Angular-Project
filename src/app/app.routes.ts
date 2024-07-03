95a401ad0bdb4d8c1e01e0e5d6b1e3fd85a0adc7
Youssef Benamor <youssef.beenamor@gmail.com>
2024-07-03 14:11:22 +0100

    add FindBookingComponent

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,10 +3,12 @@ import { LoginComponent } from './pages/auth/login/login.component';
 import { RegistrationComponent } from './pages/auth/registration/registration.component';
 import { HomeComponent } from './pages/home/home.component';
 import { RoomListingComponent } from './pages/room/room-listing/room-listing.component';
+import { FindBookingComponent } from './pages/booking/find-booking/find-booking.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegistrationComponent },
   { path: 'browse-all-rooms', component: RoomListingComponent },
+  { path: 'find-booking', component: FindBookingComponent },
 ];
---
+++ src/app/pages/booking/find-booking/find-booking.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-find-booking',
+  standalone: true,
+  imports: [],
+  templateUrl: './find-booking.component.html',
+  styleUrl: './find-booking.component.css'
+})
+export class FindBookingComponent {
+
+}
